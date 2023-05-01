e4252b645918c6a0ddf698cb18b4937316a5d108
ElenaBagriy <elena.bagriy1989@gmail.com>
2023-05-01 20:37:07 +0200

    update

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
         <BrowserRouter
-        // basename="/test-task-goit"
+        basename="/test-task-goit"
         >
           <App />
         </BrowserRouter>
